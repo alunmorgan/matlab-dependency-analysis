b86529971fb352980b13835e10fc666ac04865d3
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-05-13 15:29:27 +0100

    Adding debug statements

--- codebase_scan_path_setup.m
+++ codebase_scan_path_setup.m
@@ -1,7 +1,9 @@
 function codebase_scan_path_setup(rep_loc_ad, rep_loc, inhouse)
 % rep_loc_ad(str): location of the analysis code
 % rep_loc(str): location of the code to be analysed.
-
+disp(['rep_loc_ad', rep_loc_ad])
+disp(['rep_loc', rep_loc])
+disp(['inhouse', inhouse])
 addpath([rep_loc_ad, '/matlab-dependency-analysis']);
 addpath([rep_loc_ad, '/matlab-dependency-analysis/support_files']);
 core_paths = get_core_paths(inhouse);
